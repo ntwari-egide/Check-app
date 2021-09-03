de9f4c2e6dbf8ab2fde8bd6ab7870eba264d363b
ntwari <ntwariegide2@gmail.com>
2021-09-03 04:10:16 +0200

    fix bug in print pdf view

--- src/components/views/Logout.js
+++ src/components/views/Logout.js
@@ -6,16 +6,16 @@ import { Table,TableHeader,TableBody,TableCell,DataTableCell } from '@david.kucs
 const LogoutComponent = () => {
 
     var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-var hour = today.getHours()
-var min = today.getMinutes()
-var sec = today.getSeconds()
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    var hour = today.getHours()
+    var min = today.getMinutes()
+    var sec = today.getSeconds()
 
-let todayDate = `${yyyy}-${mm}-${dd} ${hour}:${min}:${sec}`;
+    let todayDate = `${yyyy}-${mm}-${dd} ${hour}:${min}:${sec}`;
 
-const styles = StyleSheet.create({
+    const styles = StyleSheet.create({
     page: {
         flexDirection: "column",
         padding: '40px',
@@ -72,7 +72,7 @@ const styles = StyleSheet.create({
                         <h1 class="m-auto pt-14 text-center">Logged out, Go <Link to={"/signin"} class="border">Login</Link> </h1>  
                         <h1 className="mt-8 text-lg text-center">Print Receipt</h1> 
 
-                        <PDFViewer>
+                        <PDFViewer className="pef-viewer">
                             <Document title={`atm receipt`} author="Check app ltd">
                                 <Page   style={styles.page} className="page" size={[580, 1350]} >
                                     <View>
@@ -80,9 +80,8 @@ const styles = StyleSheet.create({
                                     </View>
                     
                                     <View style={styles.leftAlignment}>
-                                        <Text style={styles.billToText}>Invoice #                   {invoicedetails.id}</Text>
-                                        <Text style={styles.billToText}>Invoice Date #          {dateParser(new Date(invoicedetails.createdAt))}</Text>
-                                        <Text style={styles.billToText}>Invoice Due Date #    {todayDate}</Text>
+                                        <Text style={styles.billToText}>Reciept Issued at:  
+                                        {todayDate}</Text>
                                     </View>
                                     <View>
                                         <Text style={{
@@ -91,27 +90,19 @@ const styles = StyleSheet.create({
                                     </View>
                                     <View style={{position: 'relative',top: '-90px'}}>
                                     <Table
-                                        data={invoicedetails.invoicedTasks}
+                                        data={JSON.parse(localStorage.getItem('receipt'))}
                                     >
                                         <TableHeader>
                                             <TableCell style={styles.tableHeader}>
-                                                QTN
+                                                TIME
                                             </TableCell>
                                             <TableCell style={styles.tableHeader}>
-                                                DESCRIPTION
-                                            </TableCell>
-                                            <TableCell style={styles.tableHeader}>
-                                                UNIT PRICE
-                                            </TableCell>
-                                            <TableCell style={styles.tableHeader}>
-                                                AMOUNT
+                                                AcTION
                                             </TableCell>
                                         </TableHeader>
                                         <TableBody>
                                             <DataTableCell getContent={(r) => 1} style={styles.tableRow}/>
-                                            <DataTableCell getContent={(r) => r.taskName} style={styles.tableRow}/>
-                                            <DataTableCell getContent={(r) => r.price} style={styles.tableRow}/>
-                                            <DataTableCell getContent={(r) => r.price} style={styles.tableRow}/>
+                                            <DataTableCell getContent={(r) => r.date} style={styles.action}/>                                            
                                         </TableBody>
                                     </Table>
                                     <Table
@@ -120,24 +111,14 @@ const styles = StyleSheet.create({
                                         <TableBody>
                                             <DataTableCell getContent={()=>{}} style={styles.tableTotalRow}/>
                                             <DataTableCell getContent={()=>{}} style={styles.tableRow}/>
-                                            <DataTableCell getContent={() => 'Total'} style={styles.tableRow}/>
-                                            <DataTableCell getContent={() => invoicedetails.total} style={styles.tableRow}/>
+                                            <DataTableCell getContent={() => 'Total Number Of Tasks'} style={styles.tableRow}/>
+                                            <DataTableCell getContent={() => JSON.parse(localStorage.getItem('receipt')).length} style={styles.tableRow}/>
                                         </TableBody>
-                                    </Table>
-                                    <Table
-                                        data={[{}]}
-                                    >
-                                        <TableBody>
-                                            <DataTableCell getContent={()=>{}} style={styles.tableTotalRow}/>
-                                            <DataTableCell getContent={()=>{}} style={styles.tableRow}/>
-                                            <DataTableCell getContent={() => 'Remaining'} style={styles.tableRow}/>
-                                            <DataTableCell getContent={() => invoicedetails.remaining} style={styles.tableRow}/>
-                                        </TableBody>
-                                    </Table>
+                                    </Table>                                    
                                     </View>
                                     <View>
-                                        <Text style={styles.billToText}>Chief Executive Officer of Law Firm</Text>
-                                        <Text>{props.location.state.ceodetails.fullName}</Text>
+                                        <Text style={styles.billToText}>Chief Executive Officer of Check.</Text>
+                                        <Text>Egide Ntwari</Text>
                                     </View>
                                 </Page>
                             </Document>
